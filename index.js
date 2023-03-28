8bb4ef86bcdf0a8d9b4d4114eb7e7a702e8415e0
Kubo <878184941@qq.com>
2023-03-28 21:08:13 +0800

    first commit.

---
+++ index.js
@@ -0,0 +1,115 @@
+// 我的第一个nodejs代码
+/* const hello = "Hello World!";
+console.log(hello); */
+
+// 读写文件 Blocking synchronous way
+// 获取系统文件模块
+const fs = require('fs');
+// 获取http模块,来进行网络通信,搭建http服务器
+const http = require('http');
+const url = require('url');
+
+////////////////////////////////////////////////////////
+// FILES
+
+// // 以同步方式读取系统文件内容
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// // 以同步方式写入内容到系统文件
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+
+// Non-blocking asynchronous way
+// Node.js的回调约定是错误先行，也就是第一个参数需要接收错误，其他才是正常的数据
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR! 🎇');
+
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('Your file has been written 😁');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file!');
+
+////////////////////////////////////////////////////////
+// SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+
+// 创建http服务器
+const server = http.createServer((requst, response) => {
+  // 对象的解构赋值
+  const { query, pathname } = url.parse(requst.url, true);
+
+  // Overview Page
+  if (pathname === '/' || pathname === '/overview') {
+    // 替换模板中的占位符,制作卡片
+    const cardsHtml = dataObject
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    response.end(output);
+
+    // Product Page
+  } else if (pathname === '/product') {
+    console.log(query);
+    response.end('This is the PRODUCT');
+
+    // API
+  } else if (pathname === '/api') {
+    response.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    response.end(data);
+
+    // Not Found
+  } else {
+    response.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    response.end('<h1>Page not found!</h1>');
+  }
+});
+
+// 监听请求
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
+
+//
